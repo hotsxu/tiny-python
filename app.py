75d7c001c961126106dbad1a8f2802293f2d0844
hotsxu <hotsxu@gmail.com>
2018-07-25 19:42:35 +0800

    1.add enetity 2.json

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, Response, jsonify
+
+from entity import AppDetailResponse
+
+
+class MyResponse(Response):
+    @classmethod
+    def force_type(cls, response, environ=None):
+        if isinstance(response, (list, dict)):
+            response = jsonify(response)
+        return super(Response, cls).force_type(response, environ)
+
 
 app = Flask(__name__)
+app.response_class = MyResponse
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/getAppDetail.do', methods=["GET"])
+def get_detail():
+    return AppDetailResponse("1", "1", "1", "1", "1", "1").__dict__
 
 
 if __name__ == '__main__':
---
+++ entity.py
@@ -0,0 +1,8 @@
+class AppDetailResponse:
+    def __init__(self, RT_F, RT_D, PPLC, DATAMAC, APP_TYPE, DTS):
+        self.RT_F = RT_F
+        self.RT_D = RT_D
+        self.PPLC = PPLC
+        self.DATAMAC = DATAMAC
+        self.APP_TYPE = APP_TYPE
+        self.DTS = DTS
