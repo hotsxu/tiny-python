1942df8e2133f2a23831f9fa59de3e0237434d94
hotsxu <hotsxu@gmail.com>
2018-08-14 18:00:38 +0800

    update

--- entity.py
+++ entity.py
@@ -8,3 +8,11 @@ class Cat:
     def __init__(self, image_id, url):
         self.image_id = image_id
         self.url = url
+
+
+# 图片实体类
+class Image:
+    def __init__(self, time_stamp, tag, url):
+        self.time_stamp = time_stamp
+        self.tag = tag
+        self.url = url
